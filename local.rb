9dd9899387f421cf276e469e42db721e29c883dd
Osamu Koga <github@osak.jp>
2024-03-10 11:49:31 +0900

    rustup

--- local.rb
+++ local.rb
@@ -2,3 +2,8 @@ execute 'install bun' do
   command 'curl -fsSL https://bun.sh/install | bash'
   not_if 'which bun'
 end
+
+execute 'install rustup' do
+  command 'curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y'
+  not_if 'which rustup'
+end
